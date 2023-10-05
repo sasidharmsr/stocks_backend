40f50f4bde3bc4e2dad5e1725454cd2ebdb7ab98
sasidharmsr <sasidharmaginam4@gmail.com>
2023-10-05 16:48:25 +0530

    db-changespool

--- db/db.js
+++ db/db.js
@@ -1,21 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password:  process.env.DATABASE_PASSWORD,
   database:  process.env.DATABASE_NAME,
   port:  process.env.DATABASE_PORT
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  
-  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
